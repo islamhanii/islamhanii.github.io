6548290274a1f2c3ef0943e6193a7a838de4172e
islam hani <36332875+islamhanii@users.noreply.github.com>
2020-10-07 16:11:36 +0200

    new website

--- Templates/first-bootstrap4-website/js/script1.js
+++ Templates/first-bootstrap4-website/js/script1.js
@@ -1,2 +1,61 @@
 /*global document, $*/
 /*jslint plusplus:true, unused:false*/
+
+function togglefide(element, start, end) {
+    if(start > end) {
+        let intervalID = window.setInterval(function() {
+            if(end >= start) {window.clearInterval(intervalID);}
+            else {
+                start -= 0.009;
+                element.style.opacity = start;
+            }
+        }, 1);
+    }
+
+    else {
+        let intervalID = window.setInterval(function() {
+            if(end <= start) {window.clearInterval(intervalID);}
+            else {
+                start += 0.009;
+                element.style.opacity = start;
+            }
+        }, 1);
+    }
+}
+
+function choosePhotos(name) {
+    "use strict";
+    let button = document.getElementById(name).parentElement.children,
+        photo = document.querySelectorAll(".photos img");
+
+    for(let i=0; i<button.length; i++) {
+        if(button[i].classList.contains("active")) {
+            button[i].classList.remove("active");
+            document.getElementById(name).classList.add("active");
+            break;
+        }
+    }
+    
+    for(let i=0; i<photo.length; i++) {
+        if(name === "all") {
+            if(photo[i].style.opacity <= 0.1) {togglefide(photo[i], 0.1, 1);}
+        }
+        else if(photo[i].classList.contains(name)) {
+            if(photo[i].style.opacity <= 0.1) {togglefide(photo[i], 0.1, 1);}
+        }
+        else {
+            if(photo[i].style.opacity >= 1) {togglefide(photo[i], 1, 0.1);}
+        }
+    }
+}
+/*
+function initailOpcity() {
+    "use strict";
+    let photo = document.querySelectorAll(".photos img");
+    for(let i=0; i<photo.length; i++) {
+        photo[i].style.opcity = 1;
+    }
+}
+
+window.onload = initailOpcity;*/
+window.addEventListener('laod', choosePhotos('all'));
