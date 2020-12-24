0d9b3af08a57a1332fc913669966adf54615bf4d
islam hani <36332875+islamhanii@users.noreply.github.com>
2020-12-24 18:36:48 +0200

    new update

--- MCQ/js/script1.js
+++ MCQ/js/script1.js
@@ -1,12 +1,10 @@
-/*global document, $*/
+/*global document, window*/
 /*jslint plusplus:true, unused:false*/
 
-"use strict";
-
-var answers = {"q1":2, "q2":1, "q3":2, "q4":2, "q5":1, "q6":2, "q7":1, "q8":1, "q9":2, "q10":1, "q11":2, "q12":2, "q13":1, "q14":1, "q15":1,
-               "q16":2, "q17":1, "q18":2, "q19":2, "q20":1, "q21":2, "q22":1, "q23":1, "q24":1, "q25":1, "q26":1, "q27":2, "q28":2, "q29":2, "q30":2, "q31":1, "q32":2, "q33":2, "q34":1, "q35":2, "q36":2, "q37":1, "q38":1, "q39":1, "q40":1, "q41":1, "q42":1, "q43":2, "q44":2, "q45":1, "q46":2, "q47":2, "q48":1}
+var answers = {"q1":2, "q2":1, "q3":2, "q4":2, "q5":1, "q6":2, "q7":1, "q8":1, "q9":2, "q10":1, "q11":2, "q12":2, "q13":1, "q14":1,  "q15":1, "q16":2, "q17":1, "q18":2, "q19":2, "q20":1, "q21":2, "q22":1, "q23":1, "q24":1, "q25":1, "q26":1, "q27":2, "q28":2, "q29":2, "q30":2, "q31":1, "q32":2, "q33":2, "q34":1, "q35":2, "q36":2, "q37":1, "q38":1, "q39":1, "q40":1, "q41":1, "q42":1, "q43":2, "q44":2, "q45":1, "q46":2, "q47":2, "q48":1};
 
 function checkAnswer(num, ques) {
+    "use strict";
     if(ques.firstElementChild.disabled !== true) {
         ques.firstElementChild.classList.add("color-input");
         
@@ -24,6 +22,7 @@ function checkAnswer(num, ques) {
 }
 
 function random() {
+    "use strict";
     let array = [];
     let question = document.getElementsByClassName("question-box");
     for(let i=0; i<question.length; i++) {
@@ -34,9 +33,9 @@ function random() {
     container.innerHTML = "";
     
     while(array.length > 0) {
-        let random = Math.floor(Math.random() * array.length);
-        container.innerHTML = container.innerHTML + array[random];
-        array.splice(random, 1);
+        let rand = Math.floor(Math.random() * array.length);
+        container.innerHTML = container.innerHTML + array[rand];
+        array.splice(rand, 1);
     }
 }
 
